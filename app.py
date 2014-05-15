b1f6056ea27b9772b5a85da48455ee21926ebcef
Menan Vadivel <menan89@gmail.com>
2014-05-14 20:59:48 -0400

    updated playlist names

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ class migrate:
       access_token = web.cookies().get('at')
       access_token_secret = web.cookies().get('ats')
       if access_token and access_token_secret:
-        self.process_playlist(api)
+        self.process_playlist(i.email, i.password, access_token, access_token_secret)
         # result = q.enqueue(self.process_playlist, i.email, i.password, access_token, access_token_secret)
 
         return render.done()
@@ -167,6 +167,8 @@ class migrate:
         tracks_string = ','.join(playlist['trackKeys'])
         songs_info = self.get_tracks_by_keys_from_rdio(tracks_string,rdio)
 
+        # print '''gonna remove playlist by name %s''' % playlist['name']
+        # self.remove_playlist_by_name(playlist['name'], googleApi, all_playlists)
 
         for key in playlist['trackKeys']:
           song = songs_info[key]
@@ -174,12 +176,11 @@ class migrate:
           track_id = self.search_song_by_name(song['name'], song['artist'] ,googleApi)
           # uses the existing playlist so that we won't have to create a new one.
           # print '''track %s id is %s''' % (song['name'], track_id)
-          self.remove_playlist_by_name(playlist['name'], googleApi, all_playlists)
-          # playlist_id = self.find_or_create_playlist_by_name(playlist['name'], googleApi, all_playlists)
-          # if track_id > 0:
-          #   googleApi.add_songs_to_playlist(playlist_id,track_id)
-          #   print '''added song %s to playlist %s''' % (song['name'], playlist['name'])
-          # sleep(2)
+          playlist_id = self.find_or_create_playlist_by_name(playlist['name'], googleApi, all_playlists)
+          if track_id > 0:
+            googleApi.add_songs_to_playlist(playlist_id,track_id)
+            print '''added song %s to playlist %s''' % (song['name'], playlist['name'])
+            # sleep(2)
       return True
     else:
       return False
